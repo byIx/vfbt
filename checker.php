a883e09c768566379f192399285ac26d9b53e649
byIx <40878600+byIx@users.noreply.github.com>
2022-09-28 22:05:23 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.0.0", "longversion":10000, "released":1664143200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/111\/" }
+{ "version":"1.1.0", "longversion":11000, "released":1664143200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2771-k4-file-buttons-in-topics\/" }
